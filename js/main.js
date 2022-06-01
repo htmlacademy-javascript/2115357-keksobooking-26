9f4e186d9a0d04db8249c07a3ffd0a69d1e3122d
sbbs8668 <sbbs8668@gmail.com>
2022-06-02 00:35:16 +0300

    linter errors correction

--- js/main.js
+++ js/main.js
@@ -1,48 +1,55 @@
-'use strict';
-
 const getRandomNumber  = (from = false, to = false, floatsNum = 0) => {
 
   if (typeof from !== 'number' || typeof to !== 'number') {
+
     return NaN;
-  };
 
-  let fromFloatsNum = 0;
-  let toFloatsNum = 0;
+  }
+
   let i = 1;
-
-  let result = [];
-
-  const getFloatsNumber = s => s.indexOf('.') > 0 ? s.length - 2 /* 2 = 0. */ : 0;
 
   from = Math.abs(from);
   to = Math.abs(to);
 
-  from > to ? [from, to] = [to, from] : false;
+  if (from > to) {
 
-  !floatsNum ? (
+    [from, to] = [to, from];
 
-    from = Math.floor(from),
-    to = Math.floor(to)
+  }
 
-  ) : (
+  if (!floatsNum) {
+
+    from = Math.floor(from);
+    to = Math.floor(to);
+
+  } else {
+
+    const getFloatsNumber = s => s.indexOf('.') > 0 ? s.length - 2 /* 2 = 0. */ : 0;
 
     /* +1 - avoids +e format */
-    fromFloatsNum = getFloatsNumber((from + 1).toString()),
-    toFloatsNum = getFloatsNumber((to + 1).toString()),
+    let fromFloatsNum = getFloatsNumber((from + 1).toString());
+    let toFloatsNum = getFloatsNumber((to + 1).toString());
 
-    !fromFloatsNum && !toFloatsNum ?
-      floatsNum = 0 :
-      i = fromFloatsNum > toFloatsNum ? 10 ** -fromFloatsNum : 10 ** -toFloatsNum
+    if (!fromFloatsNum && !toFloatsNum) {
 
-  );
+      floatsNum = 0;
+
+    } else {
+
+      i = fromFloatsNum > toFloatsNum ? 10 ** -fromFloatsNum : 10 ** -toFloatsNum;
+
+    }
+
+  }
+
+  let result = [];
 
   while (from <= to) {
 
     result.push(to);
-
     to -= i;
 
-  };
+  }
 
   result = result[Math.floor(Math.random() * result.length)];
 
@@ -50,7 +57,9 @@ const getRandomNumber  = (from = false, to = false, floatsNum = 0) => {
 
 };
 
-console.log(getRandomNumber());
+getRandomNumber();
+
+/*console.log(getRandomNumber());
 console.log(getRandomNumber('dd', 1236));
 console.log(getRandomNumber(1, []));
 
@@ -59,4 +68,4 @@ console.log(getRandomNumber(13, 1236));
 console.log(getRandomNumber(0.01000, 0.001, 3));
 console.log(getRandomNumber(0.01000, 0.0001, 4));
 console.log(getRandomNumber(0.000000000009, -0.00003, 23));
-console.log(getRandomNumber(0.000000000009, -2));
+console.log(getRandomNumber(-2, 0.000000000009));*/

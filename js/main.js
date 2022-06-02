9ac5baf41fbd114101ccedc76092ce1a94080721
sbbs8668 <sbbs8668@gmail.com>
2022-06-02 19:42:08 +0300

    getRandomNumber() v2.0

--- js/main.js
+++ js/main.js
@@ -12,12 +12,24 @@ const getRandomNumber  = (from = false, to = false, floatsNum = 0) => {
     return NaN;
 
   }
+
   /**/
 
-  /* i - an increment to calculate the range of numbers,
-  !floatsNum ? i = 1 : i = 0.001, or 0,00001 etc. */
-  let i = 1;
-  const pow10x = 10;
+  /* convert a float to an int and back */
+  const getXtoPow = (y) => 10 ** y;
+
+  const getFloatsNumber = (s) => {
+
+    /* to avoid +e format +100,
+    +100 doesn't get (e.g.) 0.59 to 0.58999999999999, +1 does */
+    s = (s + 100).toString();
+
+    /* gets a number of digits after the period (23.34455 - 5) */
+    return s.indexOf('.') > 0 ? s.length - s.indexOf('.') - 1 : 0;
+
+  };
+
+  /**/
 
   /* normalize the order of the vars */
   from = Math.abs(from);
@@ -28,63 +40,29 @@ const getRandomNumber  = (from = false, to = false, floatsNum = 0) => {
     [from, to] = [to, from];
 
   }
+
   /**/
 
-  if (!floatsNum) {
+  /* result = Round(from + randomNumberFromDifference(from-to)Range) */
 
-    /*if ints*/
+  /* get range */
+  let result = to - from;
 
-    from = Math.floor(from);
-    to = Math.floor(to);
+  /* range to int (e.g. 32.00231 * 100000) */
+  result = floatsNum ? result * getXtoPow(getFloatsNumber(from)) : result;
 
-  } else {
+  /* random number from range (0-range) */
+  result = Math.floor(Math.random() * (result + 1));
 
-    /*if floats*/
+  /* random number back to float (e.g. 100231 / 100000) */
+  result = floatsNum ? result / getXtoPow(getFloatsNumber(from)) : result;
 
-    const getFloatsNumber = (s) => {
+  /* the sum: */
+  result = from + result;
 
-      /* +1 - avoids +e format */
-      s = (s + 1).toString();
-
-      /* number of digits after the period */
-      return s.indexOf('.') > 0 ? s.length - s.indexOf('.') - 1 : 0;
-
-    };
-
-    /* get a number of digits after the period to calculate the increment*/
-    const fromFloatsNum = getFloatsNumber(from);
-    const toFloatsNum = getFloatsNumber(to);
-
-    if (!fromFloatsNum && !toFloatsNum) {
-
-      /* if none of the vars is a float, gets it back to ints, i remains 1 */
-      floatsNum = 0;
-
-    } else {
-
-      /* gets the value of the increment */
-      i = fromFloatsNum > toFloatsNum ? pow10x ** -fromFloatsNum : pow10x ** -toFloatsNum;
-
-    }
-
-  }
-
-  let result = [];
-
-  while (from <= to) {
-
-    /* gets the array of all values in the range */
-    result.push(to);
-    to -= i;
-
-  }
-
-  /* gets a random value */
-  result = result[Math.floor(Math.random() * result.length)];
-
-  /* returns a rounded float or a whole int */
-  return floatsNum ? Math.round(result * pow10x ** floatsNum) / pow10x ** floatsNum : result;
+  /* round to floatsNum, e.g.: round(12.099799 * 100) = 1210 / 100 = 12.10 */
+  return floatsNum ?
+    Math.round(result * getXtoPow(floatsNum)) / getXtoPow(floatsNum) :
+    Math.floor(result);
 
 };
-
-getRandomNumber();

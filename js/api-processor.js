53f72d831da85575ed2dc907dc4bfa0aea2f8f1f
sbbs8668 <sbbs8668@gmail.com>
2022-06-06 12:25:42 +0300

    gets data from api

---
+++ js/api-processor.js
@@ -0,0 +1,113 @@
+/* functions START */
+import   {getRandomNumber}    from './functions.js';
+/* functions END */
+
+/* get api data v1.0 */
+const getAPIData = () => {
+  return new Promise((sR)=> {
+    setTimeout(() => {
+      const ADS_DATA = JSON.stringify({
+        PARAM: {
+          avatar: {
+            countFrom: 0,
+            countTo:   0,
+            partLn:   2,
+            partPrfx:  0,
+            urlMask: {
+              start:      'img/avatars/user',
+              end:        '.png',
+            },
+          },
+          title: {
+            wordsLenFr:      3,
+            wordsLenTo:      10,
+            wordsNumFr:      3,
+            wordsNumTo:      10,
+            sentencesNumFr:  1,
+            sentencesNumTo:  1,
+          },
+          description: {
+            wordsLenFr:      2,
+            wordsLenTo:      9,
+            wordsNumFr:      4,
+            wordsNumTo:      12,
+            sentencesNumFr:  4,
+            sentencesNumTo:  12,
+          },
+          photos: {
+            numberFrom: 1,
+            numberTo: 20,
+            urlBase: 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/',
+            files: [
+              'duonguyen-8LrGtIxxa4w.jpg',
+              'brandon-hoogenboom-SNxQGWxZQi0.jpg',
+              'claire-rendall-b6kAwr1i0Iw.jpg.',
+            ],
+          },
+        },
+        PROPERTY: {
+          coordinates: {
+            lat: {
+              x: 35.64999,
+              y: 35.70001,
+            },
+            lng: {
+              x: 139.69999,
+              y: 139.80001,
+            },
+            floatsNumber: 5,
+          },
+          price: {
+            from: 300,
+            to: 300,
+            def: 1000,
+          },
+          roomsNumber: {
+            from: 1,
+            to: 10,
+            def: 1,
+          },
+          type: {
+            names: [
+              'palace',
+              'flat',
+              'house',
+              'bungalow',
+              'hotel',
+            ],
+          },
+          features: {
+            names: [
+              'wifi',
+              'dishwasher',
+              'parking',
+              'washer',
+              'elevator',
+              'conditioner',
+            ],
+          },
+          time: {
+            checkin: [
+              '12:00',
+              '13:00',
+              '14:00',
+            ],
+            checkout: [
+              '12:00',
+              '13:00',
+              '14:00',
+            ],
+          },
+          capacity: {
+            from: 1,
+            to: 10,
+            def: 2,
+          },
+        },
+      });
+      sR(ADS_DATA);
+    }, getRandomNumber(100, 1500));
+  });
+};
+
+export {getAPIData};
